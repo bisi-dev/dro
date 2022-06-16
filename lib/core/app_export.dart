5f4b8beb031971ce9d5c9d9f7cef7ccac6d3e10b
Ayodeji Olabisi <yinka.olabisi15@gmail.com>
2022-06-16 08:04:42 +0100

    feat: define folder structure

---
+++ lib/core/app_export.dart
@@ -0,0 +1,7 @@
+export 'package:dro/core/constants/image_constant.dart';
+export 'package:dro/core/constants/color_constant.dart';
+
+export 'package:dro/core/theme/app_style.dart';
+export 'package:dro/core/theme/app_decoration.dart';
+
+export 'package:dro/routes/app_routes.dart';
---
+++ lib/core/theme/app_decoration.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:dro/core/app_export.dart';
+
+class AppDecoration {
+}
